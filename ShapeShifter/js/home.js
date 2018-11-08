c982826319134c86b67018ca090675d6511ebd1d
Zaykha San <zaykha@zaykha-macbookpro.roam.corp.google.com>
2018-11-08 13:17:27 -0800

    Implemented user input/gameplay screen; Refactored code.

--- ShapeShifter/js/home.js
+++ ShapeShifter/js/home.js
@@ -10,13 +10,28 @@ function loadShapeRelations(shapeRelations) {
     // Shuffle the shapeRelations array first, fill in the shapeRelation divs, then change the displays of the
     // first and second ContainerDivs.
     var shuffledRelations = shuffle(shapeRelations);
-    document.getElementById('firstShapeRelation').setAttribute('src', shuffledRelations[0]);
-    document.getElementById('secondShapeRelation').setAttribute('src', shuffledRelations[1]);
-    document.getElementById('thirdShapeRelation').setAttribute('src', shuffledRelations[2]);
-    document.getElementById('fourthShapeRelation').setAttribute('src', shuffledRelations[3]);
+    for (var i = 1; i < 5; ++i) {
+        document.getElementById('shapeRelation' + i).setAttribute('src', shuffledRelations[i - 1]);
+    }
 
     document.getElementById('firstContainerDiv').style.display = 'none';
-    document.getElementById('secondContainerDiv').style.display = 'block';
+    document.getElementById('secondContainerDiv').style.display = 'flex';
+
+    window.setTimeout(function() {loadAvailableInputs(availableShapes)}, 3000);
+}
+
+function loadAvailableInputs(availableShapes) {
+    // Shuffle the input array and load the available shapes into the inputShapeDivs.
+    var shuffledInputs = shuffle(availableShapes);
+    for (var i = 1; i < 5; ++i) {
+        document.getElementById('inputShape' + i).setAttribute('src', shuffledInputs[i - 1]);
+        document.getElementById('inputShape' + i).onclick = function(event) {
+            appendInput(event);
+        };
+    }
+
+    document.getElementById('secondContainerDiv').style.display = 'none';
+    document.getElementById('thirdContainerDiv').style.display = 'flex';
 }
 
 function shuffle(array) {
@@ -39,13 +54,39 @@ function shuffle(array) {
     return array;
 }
 
+function appendInput(event) {
+    // Stop event propagation from bubbling to parent.
+    event.stopPropagation();
+
+    let newInputDiv = document.createElement('div');
+    newInputDiv.setAttribute("class", "inputShapeDiv");
+
+    // Create a div to hold the icons.
+    let inputShape = document.createElement('img');
+    inputShape.setAttribute("class", "inputShape");
+    inputShape.setAttribute("src", event.target.src);
+    inputShape.style.cursor = "unset";
+    newInputDiv.appendChild(inputShape);
+
+    let pastInputDiv = document.getElementById('pastInputDiv');
+    pastInputDiv.appendChild(newInputDiv);
+    pastInputDiv.scroll(pastInputDiv.scrollWidth, 0);
+}
+
+// The urls are respective to the html files.
+var availableShapes = [
+    './shapes/inputs/black_circle.png',
+    './shapes/inputs/black_square.png',
+    './shapes/inputs/blue_circle.png',
+    './shapes/inputs/blue_square.png'];
+
+var shapeRelations = [
+    './shapes/relations/sprite.png',
+    './shapes/relations/sprite2.png',
+    './shapes/relations/sprite3.png',
+    './shapes/relations/sprite4.png'];
+
 window.onload = function() {
-    // The urls are respective to the html files.
-    var availableShapes = ['./shapes/sphere.png', './shapes/square.png'];
-
-    // TODO: replace with actual shape relation sprites.
-    var shapeRelations = ['./shapes/sprite.png', './shapes/sprite2.png', './shapes/sprite3.png', './shapes/sprite4.png'];
-
     loadShapes(availableShapes);
     window.setTimeout(function() {loadShapeRelations(shapeRelations)}, 3000);
 }
